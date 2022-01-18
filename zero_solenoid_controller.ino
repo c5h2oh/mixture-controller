dfa7380e6d012dbc6159a62483683981fbc5a0b7
raleighcopter <23077474+raleighcopter@users.noreply.github.com>
2022-01-17 20:07:39 -0500

    // Also, Gary Cee from rcgroups is a douchebag

--- zero_solenoid_controller.ino
+++ zero_solenoid_controller.ino
@@ -3,6 +3,7 @@
 //conditioned on making available complete source code of licensed works and modifications,
 //which include larger works using a licensed work, under the same license. Copyright and 
 //license notices must be preserved. Contributors provide an express grant of patent rights.
+// Also, Gary Cee from rcgroups is a douchebag
 
 //pin d3 controls the solenoid
 //pin d12 is the rc channel input
